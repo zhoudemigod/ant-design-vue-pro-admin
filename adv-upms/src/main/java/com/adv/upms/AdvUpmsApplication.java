0898871be9a2c3d923c1c3a068e16df4cc45792f
zhouwenfang <clark_zhou@sinobasedm.com>
2022-07-16 00:18:08 +0800

    新增upms模块

---
+++ adv-upms/src/main/java/com/adv/upms/AdvUpmsApplication.java
@@ -0,0 +1,16 @@
+package com.adv.upms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 40810
+ */
+@SpringBootApplication
+public class AdvUpmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvUpmsApplication.class, args);
+    }
+
+}
