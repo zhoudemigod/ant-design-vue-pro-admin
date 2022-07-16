f888f50b6b27f7263232041c60e66670895fbb92
zhouwenfang <clark_zhou@sinobasedm.com>
2022-07-16 23:11:26 +0800

    新增网关服务

---
+++ adv-gateway/src/main/java/com/adv/gateway/AdvGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.adv.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvGatewayApplication.class, args);
+    }
+
+}
